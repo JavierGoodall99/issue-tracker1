aa07f6aceba3e2f9aa2aca4502d7f2410ea45f20
Javier <javier@databalk.nu>
2024-01-17 16:59:01 +0200

    Show the issues

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,12 +4,14 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-type PrismaClientSingleton =  ReturnType<typeof prismaClientSingleton>
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClientSingleton | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
