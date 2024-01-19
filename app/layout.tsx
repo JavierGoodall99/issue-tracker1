1d2cf662e559b56deab4ebee92b98cd1fcab9545
Javier <javier@databalk.nu>
2024-01-19 10:16:31 +0200

    Set up React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import '@radix-ui/themes/styles.css';
-import './theme-config.css';
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Container, Theme } from '@radix-ui/themes';
+import { Container, Theme } from "@radix-ui/themes";
 import NavbBar from "./NavBar";
-import AuthProvider from './auth/Provider';
+import AuthProvider from "./auth/Provider";
+import QueryClientProvider from "./QueryClientProvider";
 
-const inter = Inter({ 
-  subsets: ["latin"] ,
-  variable: '--font-inter',
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -25,17 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-      <Theme appearance="light" accentColor="violet">
-        <NavbBar />
-        <main className='p-5'>
-          <Container>
-          {children}
-          </Container>
-         </main>
-        </Theme>
-          
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme appearance="light" accentColor="violet">
+              <NavbBar />
+              <main className="p-5">
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
