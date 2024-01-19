26254cb98fb26684ebc98da0b24f13c2b42e71e4
Javier <javier@databalk.nu>
2024-01-19 11:30:27 +0200

    Enhanced the Api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,8 +1,8 @@
-import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import authOptions from "../../auth/authOptions";
+import { patchIssueSchema } from "@/app/validationSchemas";
 
 export async function PATCH(
     request: NextRequest, 
@@ -12,29 +12,43 @@ export async function PATCH(
         if (!session) return NextResponse.json({}, { status: 401 })
         
         const body = await request.json();
-        const validation = issueSchema.safeParse(body);
+        const validation = patchIssueSchema.safeParse(body);
         if (!validation.success) {
             return NextResponse.json(validation.error.format(), { status: 400 })
         }
 
-        const issue = await prisma.issue.findUnique({
-            where: { id: parseInt(params.id) }
-        })
-        if (!issue) {
-            return NextResponse.json({ error: 'Invalid issue'}, { status: 400 })
+        const { assignedToUserId, title, description } = body;
+
+        if (assignedToUserId) {
+          const user = await prisma.user.findUnique({
+            where: { id: assignedToUserId },
+          });
+          if (!user)
+            return NextResponse.json(
+              { error: "Invalid user." },
+              { status: 400 }
+            );
         }
 
+        const issue = await prisma.issue.findUnique({
+            where: { id: parseInt(params.id) },
+          });
+          if (!issue)
+            return NextResponse.json({ error: "Invalid issue" },
+            { status: 404 }
+          );
+      
         const updatedIssue = await prisma.issue.update({
-            where: { id: issue.id },
-            data: {
-                title: body.title,
-                description: body.description
-            }
-        })
+          where: { id: issue.id },
+          data: {
+            title,
+            description,
+            assignedToUserId
+          },
+        });
 
-        return NextResponse.json(updatedIssue)
+        return NextResponse.json(updatedIssue);
     }
-
 
     export async function DELETE(
         request: NextRequest, 
